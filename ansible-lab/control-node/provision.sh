620b63a2288a9c67aaff0df53515525b57ab5d05
hugoabreu1002 <hugoabreu1002@gmail.com>
2022-04-22 01:35:01 -0300

    control node ansible

---
+++ ansible-lab/control-node/provision.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo yum -y install epel-release
+echo "installing ansible"
+sudo yum -y install ansible
+
+cat <<EOT >> /etc/hosts
+192.168.1.2 control-node
+192.168.1.3 app1
+192.168.1.4 db01
+EOT
